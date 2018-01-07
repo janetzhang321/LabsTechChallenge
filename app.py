411244a8266531cef6d7f12c807a4aedc01f3ef6
Janet Zhang <janetzhang321@gmail.com>
2018-01-07 02:42:49 -0500

    added name,fav color, and fav pic

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/jz2896', methods=['GET'])
+def jz2896():
+    return render_template('janet.html', src='static/windows.jpg')
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
